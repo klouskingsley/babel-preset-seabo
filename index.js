a825d857a41e2b891003693c274c10d01ac81913
harry <harry_tse@163.com>
2018-06-25 17:01:07 +0800

    init preset

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-module.exports = function (context, options) {
+module.exports = function (context, options = {}) {
+  let browsers = ["last 2 versions", "ie >= 9"]
+  if (Array.isArray(options.browsers)) {
+    browsers = options.browsers
+  }
+
   return {
     presets: [
-
+      [
+        require('@babel/preset-env'),
+        {
+          "modules": false,
+          "targets": {
+              "browsers": browsers
+          }
+        }
+      ],
+      require('@babel/preset-react'),
     ],
     plugins: [
-      
+      require('@babel/plugin-transform-runtime'),
+      require('@babel/plugin-syntax-dynamic-import'),
+      [require('@babel/plugin-proposal-decorators'), { "legacy": true }],
+      [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+      [require('babel-plugin-import'), { "libraryName": "antd"}, "ant"],
+      [require('babel-plugin-import'), { "libraryName": "antd-mobile"}, "ant-mobile"]
     ]
   }
 }
